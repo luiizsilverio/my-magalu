58d56cf621d071a93b2f12cca732fac6c1b1e331
luiizsilverio <luiiz.silverio@gmail.com>
2022-08-30 20:30:51 -0300

    cart-context Ok

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,11 +1,20 @@
+import { useContext } from 'react';
 import shoppingBag from '../../assets/shopping-bag.png';
+import { CartContext } from '../../context/cart-context';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
+  const { itemCount } = useContext(CartContext);
+  console.log(itemCount)
+
   return (
     <div className="cart-container">
       <img src={shoppingBag} alt="sacola" />
-      <span className='cart-count'> 5 </span>
+      {
+        itemCount > 0 && (
+          <span className='cart-count'>{ itemCount }</span>
+        )
+      }
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import ProductsContextProvider from './context/products-context';
 
+import ProductsContextProvider from './context/products-context';
+import CartContextProvider from './context/cart-context';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <ProductsContextProvider>
-      <App />
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
     </ProductsContextProvider>
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
